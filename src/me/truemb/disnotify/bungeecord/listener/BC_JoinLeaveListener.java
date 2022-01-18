a5eb2ab85f51343d6657328f4f0ea1a93c4bc725
TrueMB <ich882777@gmail.com>
2022-01-18 20:42:51 +0100

    Cleaner Placeholder Value

--- src/me/truemb/disnotify/bungeecord/listener/BC_JoinLeaveListener.java
+++ src/me/truemb/disnotify/bungeecord/listener/BC_JoinLeaveListener.java
@@ -68,7 +68,7 @@ public class BC_JoinLeaveListener implements Listener{
 			HashMap<String, String> placeholder = new HashMap<>();
 			placeholder.put("Player", p.getName());
 			placeholder.put("UUID", uuid.toString());
-			placeholder.put("server", e.getTarget().getName());
+			placeholder.put("server", server);
 			
 			if(this.configManager.useEmbedMessage(FeatureType.PlayerJoinLeave)) {
 				this.discordManager.sendEmbedMessage(channelId, uuid, "PlayerLeaveEmbed", placeholder);
